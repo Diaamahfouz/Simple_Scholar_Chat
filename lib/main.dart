02e541efdafc6e55dd58643169954ed304f0536e
Diaa Mahfouz <diaamahfouz229@gmail.com>
2024-10-05 17:47:31 +0300

    integrate with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:simple_chat/firebase_options.dart';
 import 'package:simple_chat/pages/login_page.dart';
 import 'package:simple_chat/pages/register_page.dart';
 
 void main() {
+  void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  
   runApp(SimpleChat());
+}
 }
 
 class SimpleChat extends StatelessWidget {
